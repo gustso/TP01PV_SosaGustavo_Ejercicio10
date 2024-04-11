9862fe28f3cd309ae35645af27da86b3ed293be4
gustso <gustso@10.7.2.213>
2024-04-11 20:56:03 -0300

    ejercicio 10

---
+++ src/ar/edu/unju/fi/ejercicio10/main/Main.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio10.main;
+
+import ar.edu.unju.fi.ejercicio10.model.Pizza;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+					
+		Menu unMenu = new Menu();	
+		Pizza pizza = new Pizza();
+		unMenu.generarMenu(pizza);		
+				
+	}
+}
